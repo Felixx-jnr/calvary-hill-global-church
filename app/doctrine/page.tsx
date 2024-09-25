8541bd215471abcf32465eb81edca08dabd1f0e8
Felixx-jnr <uyuoukoh@gmail.com>
2024-09-25 02:07:05 +0100

    Implement navbar layout for all screen sizes

---
+++ app/doctrine/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const doctrine = () => {
+  return <div>doctrine</div>;
+};
+
+export default doctrine;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,22 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import Image from "next/image";
+
+import { Button } from "./ui/button";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 // Links object
 const navigationLinks = [
-  {
-    route: "/about",
-    label: "About",
-  },
   {
     route: "/sermons",
     label: "Sermons",
@@ -43,8 +52,14 @@ const navigationLinks = [
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const [show, setShow] = useState(false);
+
   // Toggle menu open/close
   const toggleMenu = () => setIsOpen(!isOpen);
+
+  const triggerShow = () => {
+    setShow(!show);
+  };
 
   // Animations for the hamburger menu
   const hamburgerLine1 = {
@@ -61,29 +76,40 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white fixed shadow-md w-full top-0 z-10">
-      <div className=" mx-auto px-6 py-4 flex justify-between items-center">
-        <h1 className="text-2xl font-bold">My Navbar</h1>
+    <nav className=" bg-maroon text-smokeWhite font-Sofia fixed shadow-md w-full top-0 z-10">
+      <div className=" py-4 flex justify-around max-mid:justify-between mx-5 max-mid:mx-5 items-center">
+        {/* LOGO */}
+        <Link
+          className="text-2xl z-10"
+          href="/"
+        >
+          <Image
+            src="/CHC-logo-white.png"
+            alt="Logo"
+            width={170}
+            height={1}
+          />
+        </Link>
 
         {/* Hamburger Menu */}
         <div
-          className="md:hidden"
+          className={`${isOpen ? "z-10 ml-auto mid:hidden absolute right-3" : " z-10 ml-auto mid:hidden max-md:absolute max-md:right-3"}`}
           onClick={toggleMenu}
         >
           <motion.div
-            className="w-6 h-0.5 bg-black my-1"
+            className="w-8 h-0.5 bg-white my-1 "
             animate={isOpen ? "open" : "closed"}
             variants={hamburgerLine1}
             transition={{ duration: 0.3 }}
           />
           <motion.div
-            className="w-6 h-0.5 bg-black my-1"
+            className="w-8 h-0.5 bg-white my-1 "
             animate={isOpen ? "open" : "closed"}
             variants={hamburgerLine2}
             transition={{ duration: 0.3 }}
           />
           <motion.div
-            className="w-6 h-0.5 bg-black my-1"
+            className="w-8 h-0.5 bg-white my-1 "
             animate={isOpen ? "open" : "closed"}
             variants={hamburgerLine3}
             transition={{ duration: 0.3 }}
@@ -91,12 +117,46 @@ const Navbar = () => {
         </div>
 
         {/* Desktop Links */}
-        <ul className="hidden md:flex space-x-6">
+        <ul className="hidden mid:flex space-x-7 ">
+          <TooltipProvider>
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger>
+                <li>
+                  <Link
+                    href="/about"
+                    className=" font-thin hover:border-b-2 pb-2 hover:text-white"
+                  >
+                    About
+                  </Link>
+                </li>
+              </TooltipTrigger>
+              <TooltipContent className="bg-maroon mt-12 pr-7">
+                <li>
+                  <Link
+                    href="/doctrine"
+                    className=" block font-thin p-2 pr-40 tracking-wide"
+                  >
+                    Doctrine
+                  </Link>
+                </li>
+
+                <li>
+                  <Link
+                    href="/leadership"
+                    className=" block font-thin p-2 tracking-wide"
+                  >
+                    Leadership
+                  </Link>
+                </li>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
           {navigationLinks.map((link) => (
             <li key={link.label}>
               <Link
                 href={link.route}
-                className="text-gray-800 hover:text-gray-500"
+                className=" font-thin hover:border-b-2 pb-2 hover:text-white"
               >
                 {link.label}
               </Link>
@@ -104,10 +164,27 @@ const Navbar = () => {
           ))}
         </ul>
 
-        <div className="max-md:hidden">
-          <button >Search</button> 
+        <div className=" flex items-center ">
+          <div>
+            <Image
+              src="/magnifying_glass.svg"
+              alt={""}
+              width={35}
+              height={10}
+              className=" mx-3 max-md:absolute max-md:right-10 top-7"
+            />
+          </div>
 
-          <Link className="max-md:hidden" href="/giving">Give Now</Link>
+          <div>
+            <Link
+              className="max-md:hidden"
+              href="/giving"
+            >
+              <Button className="bg-black rounded-none px-12 py-6 font-bold tracking-widest font-Sofia">
+                GIVE NOW
+              </Button>
+            </Link>
+          </div>
         </div>
 
         {/* Mobile Dropdown Menu */}
@@ -117,24 +194,66 @@ const Navbar = () => {
             animate={{ y: 0, opacity: 1 }}
             exit={{ y: "-100%", opacity: 0 }}
             transition={{ duration: 0.5 }}
-            className="absolute top-full left-0 w-full bg-white shadow-md md:hidden"
+            className="absolute top-0 left-0 w-full h-screen bg-darkBrown shadow-md mid:hidden overflow-y-auto"
           >
-            {navigationLinks.map((link) => (
-              <ul>
-              <li
-                key={link.label}
-                className="border-b"
-              >
-                <Link
-                  href={link.route}
-                  className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                  onClick={toggleMenu}
+            <ul className="flex flex-col items-center justify-center mt-28 text-[#CAC2C0] ">
+              <li className="flex items-center ">
+                <div className=" font-bold text-2xl my-3">
+                  <Link
+                    className="text-3xl my-3 font-bold hover:text-[#fff] mr-3"
+                    href="/about"
+                    onClick={toggleMenu}
+                  >
+                    About
+                  </Link>
+                </div>
+
+                <button
+                  onClick={triggerShow}
+                  className=" "
                 >
-                  {link.label}
-                </Link>
+                  <FontAwesomeIcon icon={faArrowRight} />{" "}
+                </button>
               </li>
-              </ul>
-            ))}
+
+              {show && (
+                <li className="">
+                  <div>
+                    <Link
+                      href="/doctrine"
+                      className=" block font-thin p-2 tracking-wide text-center hover:text-[#fff]"
+                      onClick={toggleMenu}
+                    >
+                      Doctrines
+                    </Link>
+                  </div>
+
+                  <li>
+                    <Link
+                      href="/leadership"
+                      className=" block font-thin p-2 tracking-wide text-center hover:text-[#fff]"
+                      onClick={toggleMenu}
+                    >
+                      Leadership
+                    </Link>
+                  </li>
+                </li>
+              )}
+
+              {navigationLinks.map((link) => (
+                <li
+                  key={link.label}
+                  className=" text-3xl my-3 font-bold hover:text-[#fff]"
+                >
+                  <Link
+                    href={link.route}
+                    onClick={toggleMenu}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </motion.ul>
         )}
       </div>
