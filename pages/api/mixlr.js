e908e5751619a9f120d8899c00aa1c52dd70f622
Felixx-jnr <uyuoukoh@gmail.com>
2025-03-24 12:32:52 +0100

    Typescript error fixed

--- pages/api/mixlr.js
+++ pages/api/mixlr.js
@@ -13,6 +13,7 @@ export default async function handler(req, res) {
     res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // Prevents browser caching
     res.status(200).json(data);
   } catch (error) {
+    console.error("Error fetching Mixlr status:", error); // Log the error
     res.status(500).json({ error: "Failed to fetch Mixlr status" });
   }
 }
