6ae37e37628d2dca73fefd33dd3b40e6230bad52
Felixx-jnr <uyuoukoh@gmail.com>
2025-06-25 14:50:31 +0100

    Remove unused formatTitle function and clean up Now Playing title styling

--- components/GlobalAudioPlayer.tsx
+++ components/GlobalAudioPlayer.tsx
@@ -8,25 +8,11 @@ const GlobalAudioPlayer = () => {
 
   if (!playingFile) return null;
 
-  const formatTitle = (description: string | null): JSX.Element | null => {
-    if (!description) return null;
-
-    const words = description.split(/\s+/);
-    const truncatedDescription = words.slice(0, 8).join(" ");
-
-    return (
-      <span className="mb-">
-        {truncatedDescription}
-        {words.length > 8 && "..."}
-      </span>
-    );
-  };
-
   return (
     <div className="right-0 bottom-0 z-50 fixed bg-darkGrey shadow-md px-2 py-2 xs:w-full sm:w-[90%] max-w-[540px]">
       <div className="mx-auto max-w-4xl">
-        <p className="mb-2 font-medium text-sm text-maroon line-clamp-1 ">
-          Now Playing: <span>  { playingFile.metadata.title} </span>
+        <p className="mb-2 font-medium text-maroon text-sm line-clamp-1">
+          Now Playing: <span> {playingFile.metadata.title} </span>
         </p>
         <AudioPlayer
           audioSrc={playingFile.url}
